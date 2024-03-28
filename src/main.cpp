5c12f0fe684ccc23d28e9416a86982d1b2142662
Louis <louismeidel@maag.com>
2024-03-28 10:21:53 +0000

    add main.cpp to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,14 @@
 /**
- * @file    LedCycle.cpp
- * @brief   Example program demonstrating the usage of the MyWS2812B library to control WS2812B LEDs.
- * @author  lusmdl
- * @date    05.02.2024
+ * @file main.cpp
+ * @brief 
  * 
- * This example initializes a WS2812B strip with 3 LEDs connected to PORTD (PIN6) of the microcontroller.
- * It cycles through RED, GREEN, and BLUE colors on the LEDs in a loop with a delay of 500ms between changes.
+ * @date 28.03.2024
+ * @author lusmdl
  */
 
 #include <MyNeoPixels.hpp>
 
-
-MyWS2812B leds(DDRD, PORTD, (uint8_t*) _SFR_MEM_ADDR(PORTD), PD6, 3);
-
-
-int main(void)
-{
-  leds.clearColor();
-  leds.show();
-
-  _delay_ms(2000);
-
-  uint32_t counter;
-  while(1)
-  {
-
-
-    uint8_t i_0 = counter % 3;
-    uint8_t i_1 = (counter+1) % 3;
-    uint8_t i_2 = (counter+2) % 3;
-
-    leds.setColor(i_0, 255, 0 , 0);
-    leds.setColor(i_1, 0, 255, 0);
-    leds.setColor(i_2, 0, 0, 255);
-    leds.show();
-    _delay_ms(500);
-    counter++;
-  }
+int main(int argc, char const *argv[]) {
+  
+  return 0;
 }
